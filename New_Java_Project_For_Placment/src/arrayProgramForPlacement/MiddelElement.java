b8a4895b8274da2f5e846618730d905d16cee57c
Abhay7898 <128139505+Abhay7898@users.noreply.github.com>
2024-03-08 19:22:57 +0530

    8-3

---
+++ New_Java_Project_For_Placment/src/arrayProgramForPlacement/MiddelElement.java
@@ -0,0 +1,9 @@
+package arrayProgramForPlacement;
+
+public class MiddelElement {
+	public static void main(String[] args) {
+		int a []={1, 2, 3, 4, 6, 7,6,9};
+		int middel=a.length/2;
+		System.out.println(a[middel]);
+	}
+}
---
+++ New_Java_Project_For_Placment/src/integerQuestionForPlacement/NumberDivide.java
@@ -0,0 +1,13 @@
+package integerQuestionForPlacement;
+
+public class NumberDivide {
+	public static void main(String[] args) {
+		int num=45;
+		int one=0;
+		while(num!=0) {
+			 one=num%10;
+			=(num*10)+one;
+			num
+		}
+	}
+}
