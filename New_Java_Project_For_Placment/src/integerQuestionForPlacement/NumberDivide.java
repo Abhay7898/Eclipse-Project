1dfbea1bbe4f2b6ca2ac58b6c5b6757a978e1366
Abhay7898 <128139505+Abhay7898@users.noreply.github.com>
2024-03-08 19:36:43 +0530

    8-3

--- New_Java_Project_For_Placment/src/integerQuestionForPlacement/NumberDivide.java
+++ New_Java_Project_For_Placment/src/integerQuestionForPlacement/NumberDivide.java
@@ -2,12 +2,6 @@ package integerQuestionForPlacement;
 
 public class NumberDivide {
 	public static void main(String[] args) {
-		int num=45;
-		int one=0;
-		while(num!=0) {
-			 one=num%10;
-			=(num*10)+one;
-			num
-		}
+		
 	}
 }
